273ae092e2676376c9f1f88412417fdfa63fbd88
AminKhorasani8910 <aminkhorasani8910@gmail.com>
2026-08-28 15:18:35 +0330

    Add files via upload

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,12 @@
-
+let menu = document.querySelector(".nav")
+let menuBtn = document.querySelector(".header__icon")
+let menuBtnIcon = document.querySelector(".header__icon i")
+menuBtn.addEventListener("click" , function () {
+    if (menuBtnIcon.classList.contains("fa-bars")){
+        menu.style.left = "0"
+        menuBtnIcon.classList ="fa fa-times"
+    } else {
+        menu.style.left = "-278.4px"
+        menuBtnIcon.classList ="fa fa-bars"
+    }
+})
